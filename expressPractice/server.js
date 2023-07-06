7d64ab7d32aa4c3cfa56ab72fa763c54a9a744ac
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-06 15:40:37 -0700

    First commit

---
+++ expressPractice/server.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+
+const app = express();
+
+
+// Define a "root" route directly on app
+// Tomorrow, we'll use best practice routing
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>');
+  });
+  
+  // Tell the app to listen on port 3000
+  // for HTTP requests from clients
+  app.listen(3000, function () {
+    console.log('Listening on port 3000');
+  });
